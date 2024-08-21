c91fcf3a0cd45489e69d8c56e5040bc89b5faa35
Michael <muxaujish@gmail.com>
2024-08-21 16:07:09 +0300

    fix double logging for aspect

--- src/main/java/ru/ylib/aspects/AuditAspect.java
+++ src/main/java/ru/ylib/aspects/AuditAspect.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Aspect
 @Component
+@EnableAspectJAutoProxy
 public class AuditAspect {
 
     @Around("execution(* ru.ylib.services.*.*(..))")
